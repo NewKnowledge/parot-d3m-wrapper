f4ea1c36935599ce96252fc2b924fce4f7cfce18
jlgleason <jeffrey.gleason.3@gmail.com>
2018-10-23 17:07:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='ParrotD3MWrapper',
     description='A thin wrapper for interacting with New Knowledge time series prediction tool Parrot',
     packages=['ParrotD3MWrapper'],
     install_requires=["typing",
-        "Parot==1.0.0"],
+        "Parrot==1.0.0"],
     dependency_links=[
         # must change 
         "git+https://github.com/NewKnowledge/sloth@d38b8892fbefb4425d211d2cd858cfad91a2113e#egg=Sloth-2.0.0"

4f8ee1291aa6721a231da9cd362f526ae3ba08bb
jlgleason <jeffrey.gleason.3@gmail.com>
2018-10-25 13:51:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='ParrotD3MWrapper',
     ],
     entry_points = {
         'd3m.primitives': [
-            'distil.parrot = ParrotD3MWrapper:Parrot'
+            'distil.Parrot = ParrotD3MWrapper:Parrot'
         ],
     },
 )

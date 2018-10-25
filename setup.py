4190d82b8393c8897323b1437bd170fe4e72e1cf
Ubuntu <jgleason@JeffreyVMDev.umi1swgzx55e3jlvq0v5igys1h.jx.internal.cloudapp.net>
2018-10-25 17:49:21 +0000

    change Sloth to version 2.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='ParrotD3MWrapper',
     description='A thin wrapper for interacting with New Knowledge time series prediction tool Parrot',
     packages=['ParrotD3MWrapper'],
     install_requires=["typing",
-        "Sloth>=2.0.0"],
+        "Sloth>=2.0.1"],
     dependency_links=[
         "git+https://github.com/NewKnowledge/sloth@jg/editArima#egg=Sloth-2.0.0"
     ],

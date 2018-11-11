260d9529641a685069df22997c16ce2081ad07e1
jlgleason <jeffrey.gleason.3@gmail.com>
2018-11-11 15:26:42 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='ParrotD3MWrapper',
     install_requires=["typing",
         "Sloth>=2.0.2"],
     dependency_links=[
-        "git+https://github.com/NewKnowledge/sloth@7a382ed33a745d6a1205c0bcfe31262332b81e1eegg=Sloth-2.0.2"
+        "git+https://github.com/NewKnowledge/sloth@6e6d30e48c38397755daa51a2350c7a321ecb1a4egg=Sloth-2.0.2"
 
     ],
     entry_points = {

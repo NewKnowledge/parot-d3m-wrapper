977a894dcc566902d929573fdbc7b697a5297021
Ubuntu <jgleason@JeffreyVMDev.umi1swgzx55e3jlvq0v5igys1h.jx.internal.cloudapp.net>
2018-12-14 16:53:06 +0000

    change install requires to ==

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='ParrotD3MWrapper',
     description='A thin wrapper for interacting with New Knowledge time series prediction tool Parrot',
     packages=['ParrotD3MWrapper'],
     install_requires=["typing",
-        "Sloth>=2.0.2"],
+        "Sloth==2.0.2"],
     dependency_links=[
         "git+https://github.com/NewKnowledge/sloth@4a4c8d3e22fec9a995dbcf7a065aa63f35bcec0f#egg=Sloth-2.0.2"
     ],
